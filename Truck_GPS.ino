8575f6551d8d0bd4134913a38583bf3a08cdc022
Carol Lynn Hazlett <carolhaz391@gmail.com>
2016-07-11 16:09:27 -0700

    Add files via upload

---
+++ Truck_GPS.ino
@@ -0,0 +1,42 @@
+#include <TinyGPS.h>
+#include <SoftwareSerial.h>
+
+TinyGPS gps;
+SoftwareSerial nss(6, 255);
+
+void setup() {
+  Serial.begin(9600);
+  nss.begin(4800);
+  Serial.println("Reading GPS");
+}
+
+void loop() {
+  bool newdata = false;
+  unsigned long start = millis();
+  while (millis() - start < 5000) {
+    if (feedgps())
+    newdata = true;
+  }
+  if (newdata) {
+    gpsdump(gps);
+  }
+
+}
+
+void gpsdump(TinyGPS &gps) {
+  float flat, flon;
+  unsigned long age;
+  gps.f_get_position (&flat, &flon, &age);
+  Serial.print(flat, 4); 
+  Serial.print(", ");
+  Serial.println(flon, 4);
+  }
+  bool feedgps() {
+    while(nss.available()) {
+      if(gps.encode(nss.read()))
+        return true;
+    }
+    return false;
+}  
+
+
